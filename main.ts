ccc08078a1fefde36e337d6807b7395b6e7200ad
flupion <126018458+flupion@users.noreply.github.com>
2023-03-08 09:09:29 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+        basic.pause(100)
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+        maqueen.motorStop(maqueen.Motors.M2)
+        basic.pause(100)
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+            maqueen.motorStop(maqueen.Motors.M1)
+            basic.pause(100)
+        }
+    }
+    maqueen.motorStop(maqueen.Motors.All)
+})
