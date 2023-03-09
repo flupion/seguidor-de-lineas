1a94bd6035280bf333406e94b5b3de39cbd04ee3
flupion <126018458+flupion@users.noreply.github.com>
2023-03-09 20:19:27 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
+let velocidad = 40
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
             basic.pause(100)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
@@ -9,7 +10,7 @@ basic.forever(function () {
             maqueen.motorStop(maqueen.Motors.M2)
             basic.pause(100)
             if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
                 maqueen.motorStop(maqueen.Motors.M1)
                 basic.pause(100)
             }
